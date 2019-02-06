ea7d2859c1aef05dcd0077e43f67e30bb7534be4
zubayer <zubayer@zubayer-M4>
2019-02-06 12:15:46 +0600

    first commit: project created

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Form submit using jQuery.ajax()
---
+++ src/main/java/com/metafour/AjaxFormSubmitApplication.java
@@ -0,0 +1,14 @@
+package com.metafour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxFormSubmitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxFormSubmitApplication.class, args);
+	}
+
+}
+
